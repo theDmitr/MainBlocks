e09a41d7c33a18d0d6077539bb757bb2333c28fc
theDmitr <dmitryfirson@gmail.com>
2022-11-22 00:29:28 +0300

    5.1 ХЗ

--- app/engine/scenes/testScene.py
+++ app/engine/scenes/testScene.py
@@ -9,7 +9,7 @@ clock = Clock()
 CENTERX = Screen.getScreenRect().centerx
 CENTERY = Screen.getScreenRect().centery
 class testScene:
-    blocks = generateLandscape(Block.WIDTH, Block.HEIGHT, 5, startY = 100)
+    blocks = generateLandscape(Block.WIDTH, Block.HEIGHT, Screen.getScreenSize()[0] // Block.WIDTH + 1, startY = 100)
     def play(surface):
         testScene.eventListener()
         testScene.renderer(surface)
@@ -18,7 +18,7 @@ class testScene:
             if event.type == pg.QUIT:
                 quit()
             if event.type == pg.MOUSEBUTTONDOWN:
-                testScene.blocks = generateLandscape(Block.WIDTH, Block.HEIGHT, 5, startY = 100)
+                testScene.blocks = generateLandscape(Block.WIDTH, Block.HEIGHT, Screen.getScreenSize()[0] // Block.WIDTH + 1, startY = 100)
     def renderer(surface):
         clock.tick(FPS)
         surface.fill((255, 255, 255))
