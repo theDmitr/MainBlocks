2701204cefa5eb1ba1d27f0c9512169396d983ae
theDmitr <dmitryfirson@gmail.com>
2022-11-21 11:53:46 +0300

    2.0

--- app/engine/scene.py
+++ app/engine/scene.py
@@ -1,12 +1,8 @@
-from pygame.time import Clock
 from pygame import display
-FPS = 60
-clock = Clock()
 class Scene:
     currentScene = None
     def setCurrentScene(scene): # setter current`s scene
         Scene.currentScene = scene
     def playCurrentScene(surface): # player current`s scene
-        clock.tick(FPS) # tick frames per second
         Scene.currentScene.play(surface) # call method `play` from current scene
         display.update() # update screen
--- app/engine/scenes/menu.py
+++ app/engine/scenes/menu.py
@@ -1,7 +1,10 @@
 import pygame as pg
+from pygame.time import Clock
 from engine.objects.button import Button
 from engine.screen import Screen
 pg.init()
+FPS = 60
+clock = Clock()
 class Menu:
     font = pg.font.Font(None, 24)
     button = Button(0, 0, 100, 40, (0, 255, 0), (255, 255, 255), "Click me!", font)
@@ -14,5 +17,6 @@ class Menu:
             if event.type == pg.QUIT:
                 quit()
     def renderer(surface):
+        clock.tick(FPS)
         surface.fill((255, 255, 255))
         Menu.button.draw(surface)
