0e557397fec251407d24c6252d5a2afb4992a78a
Sinus44 <59234792+Sinus44@users.noreply.github.com>
2022-11-23 10:53:07 +0300

    5.2 fix link

--- app/engine/objects/blocks/block.py
+++ app/engine/objects/blocks/block.py
@@ -4,7 +4,7 @@ class Block:
     WIDTH, HEIGHT = 40, 40
     def __init__(self, x, y, texture):
         self.rect = pg.Rect(x, y, Block.WIDTH, Block.HEIGHT)
-        self.texture1 = pg.transform.scale(pg.image.load(rf"app/assets/textures/blocks/{texture}.png"), (self.WIDTH, self.HEIGHT))
+        self.texture1 = pg.transform.scale(pg.image.load(rf"assets\textures\blocks\{texture}.png"), (self.WIDTH, self.HEIGHT))
     def getPos(self):
         return (self.rect.x, self.rect.y)
     def move(self, x, y):
